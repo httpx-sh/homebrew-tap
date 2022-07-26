abb40dfd65c9d13367ca5275245cdc4f2161ff3d
Libing Chen <libing.chen@gmail.com>
2022-07-26 15:28:36 -0700

    Update to version 0.35.0

--- Formula/httpx.rb
+++ Formula/httpx.rb
@@ -1,18 +1,18 @@
 class Httpx < Formula
     desc 'httpx - CLI to run HTTP file'
     homepage 'https://github.com/httpx-sh/httpx'
-    version '0.34.0'
+    version '0.35.0'
     license 'Apache-2.0'
   
     depends_on 'grpcurl'
   
     if OS.mac? && Hardware::CPU.intel?
-      url 'https://github.com/servicex-sh/httpx/releases/download/v0.34.0/httpx-0.34.0-x86_64-apple-darwin.tar'
-      sha256 'c9e97642ff12fc68b89ea284091921e1d21e56e3b4a4da52265a145415b72c02'
+      url 'https://github.com/servicex-sh/httpx/releases/download/v0.35.0/httpx-0.35.0-x86_64-apple-darwin.tar'
+      sha256 '4310a388ff1a40140a2c699b0da26a6d7f0fb774ea611155f438ce870518ae33'
     end
     if OS.mac? && Hardware::CPU.arm?
-      url 'https://github.com/servicex-sh/httpx/releases/download/v0.34.0/httpx-0.34.0-x86_64-apple-darwin.tar'
-      sha256 'c9e97642ff12fc68b89ea284091921e1d21e56e3b4a4da52265a145415b72c02'
+      url 'https://github.com/servicex-sh/httpx/releases/download/v0.35.0/httpx-0.35.0-x86_64-apple-darwin.tar'
+      sha256 '4310a388ff1a40140a2c699b0da26a6d7f0fb774ea611155f438ce870518ae33'
     end
   
     def install
